4bb972425107292b2899cbc26058ad8e34a248aa
Gaurav Keswani <gkeswani14@gmail.com>
2019-06-28 17:54:39 +0530

    fix bug

---
+++ precache-manifest.0e32c441164aab5fc06f8c8734d5d0f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16e9c30223f880d7b33fdf25790440ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca45cd2febdfb522f5f1",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "ca45cd2febdfb522f5f1",
+    "url": "/static/js/main.e97ed2b2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
